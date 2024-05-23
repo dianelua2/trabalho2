93e57dce37c833cde1e489410ea665c8e646604e
dianelua2 <claudianeaprocha@gmail.com>
2024-05-22 22:17:48 -0300

    Add files via upload
    envio do trabalho

---
+++ exercicio3a/script.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo!");
+let num1=parseInt(prompt("informe valor1"));
+let num2=parseInt(prompt("informe valor2"));
+let maior, menor, resultado;
+
+if(num1>num2){maior=num1;menor=num2; 
+}else{
+ maior=num2; menor=num1};
+  
+resultado=maior-menor
+alert(resultado)  
---
+++ exercicio3b/script.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo!"); 
+
+let num = parseInt(prompt("informe valor"));
+
+if(num<0){
+	num2=num*(-1); 
+	alert(num2);
+	}
+else{
+	alert(num); 
+	}
