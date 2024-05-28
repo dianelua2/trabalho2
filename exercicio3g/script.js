7eaf04dc81dcdf1e64a6859df082677d69a0b4e8
dianelua2 <claudianeaprocha@gmail.com>
2024-05-28 20:05:20 -0300

    Add files via upload

--- exercicio3g/script.js
+++ exercicio3g/script.js
@@ -1,1 +1,23 @@
-alert("Bem-vindo!"); 
+alert("Bem-vindo!");
+let valor1 = Number(prompt("informe o valor 1"));
+let valor2 = Number(prompt("informe o valor 2"));
+let valor3 = Number(prompt("informe o valor 3"));
+let valor4 = Number(prompt("informe o valor $"));
+let resultado = [];
+if(valor1%2==0 && valor1%3==0){
+   resultado.push(valor1);
+}
+if(valor2%2==0 && valor2%3==0){
+   resultado.push(valor2);
+}
+if(valor3%2==0 && valor3%3==0){
+   resultado.push(valor3);
+}
+if(valor4%2==0 && valor4%3==0){
+   resultado.push(valor4);
+}
+if(resultado.length<1){
+   alert("valor inválido");
+}
+alert(resultado);
+ 
---
+++ exercício3m/script.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo!"); 
+
+let nome = prompt("Informe o nome");
+let sexo = prompt("Informe o sexo");
+
+if((sexo.toLowerCase() == "masculino")){
+   alert(`Ilmo. Sr. ${nome}`);
+
+}else{
+   alert(`Ilma. Sra. ${nome}`);
+}
